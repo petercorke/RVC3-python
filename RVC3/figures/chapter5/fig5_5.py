da7abc3381a59e0dfefdabb82b613b0509c0f280
Peter Corke <peter.i.corke@gmail.com>
2021-10-02 15:51:04 +1000

    add legend use new rtb data file access function

--- RVC3/figures/chapter5/fig5_5.py
+++ RVC3/figures/chapter5/fig5_5.py
@@ -4,17 +4,25 @@ import rvcprint
 import numpy as np
 import matplotlib.pyplot as plt
 from roboticstoolbox import *
+from spatialmath import base
 
-house = rtb_loadmat('data/house.mat')
+house = rtb_load_matfile('data/house.mat')
 floorplan = house['floorplan']
 
 places = house['places'];
 print(places._fieldnames)
 
+
+
 bug = Bug2(occgrid=floorplan)
 bug.plot()
 
-p = bug.run(places.br3, places.kitchen, animate=True, trail=True)
+p = bug.run(places.br3, places.kitchen, animate=True, pause=0, trail=True, label='path')
 print(p.shape)
+
+for place in places._fieldnames:
+    base.plot_point(places.__dict__[place], 'b*', text=place)
+
+plt.legend()
 
 rvcprint.rvcprint()
