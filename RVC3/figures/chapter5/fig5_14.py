3e4c1d69af5438b049b5314165246f0f2d6714ab
Peter Corke <peter.i.corke@gmail.com>
2021-10-02 15:48:38 +1000

    add circles, fix scaling

--- RVC3/figures/chapter5/fig5_14.py
+++ RVC3/figures/chapter5/fig5_14.py
@@ -4,6 +4,7 @@ import rvcprint
 import numpy as np
 import matplotlib.pyplot as plt
 from roboticstoolbox import *
+from spatialmath import base
 
 house = rtb_load_matfile('data/house.mat')
 floorplan = house['floorplan']
@@ -11,14 +12,19 @@ places = house['places']
 
 dx = DistanceTransformPlanner(occgrid=floorplan)
 dx.plan(places.kitchen)
-dx.plot()
+# dx.plot()
 # plt.show(block=True)
 
 p = dx.query(places.br3)
 print(p.shape)
 
-plt.clf()
+# plt.clf()
 dx.plot(p, background=True)
+
+base.plot_circle(15, 'k', centre=(5, 103), linewidth=3)
+base.plot_circle(15, 'k', centre=(590, 206), linewidth=3)
+
+plt.gca().autoscale('both')
 
 rvcprint.rvcprint()
 
