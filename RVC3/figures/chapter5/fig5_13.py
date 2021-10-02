0247d94fb35e7766f3ad1b59f4f5fe4f713a3ee2
Peter Corke <peter.i.corke@gmail.com>
2021-10-02 15:47:55 +1000

    get decent looking 3d cost surface

--- RVC3/figures/chapter5/fig5_13.py
+++ RVC3/figures/chapter5/fig5_13.py
@@ -9,8 +9,8 @@ from spatialmath import base
 
 # without obstacles
 floorplan = np.zeros((6, 6))
-# dx0 = DistanceTransformPlanner(occgrid=floorplan, metric='euclidean')
-# dx0.plan((1,1))
+dx0 = DistanceTransformPlanner(occgrid=floorplan, metric='euclidean')
+dx0.plan((1,1))
 
 # add obstacles
 floorplan[2:5, 3:5] = 1
@@ -20,62 +20,31 @@ floorplan[3:5, 2] = 1
 dx = DistanceTransformPlanner(occgrid=floorplan, metric='euclidean')
 dx.plan((1,1))
 
-# ax = dx.plot_3d()
-# dx.plot_bg()
 ax = base.plotvol3()
-ax.view_init(30, -122)
-# ax = base.plotvol3()
-ax.set_xlim(-1, 7)
-ax.set_ylim(-1, 7)
 
-# D = dx0.distancemap.copy()
-# print(D)
+D = dx0.distancemap.copy()
 X, Y = np.meshgrid(np.arange(0,6), np.arange(0,6))
-# Xi, Yi = np.meshgrid(np.arange(-0.5, 7), np.arange(-0.5, 7))
 
-# from scipy import interpolate
+from scipy import interpolate
 
-# interp = interpolate.interp2d(X, Y, D, kind='linear')
+# interpolate distance transform without obstacles
+Xi, Yi = np.meshgrid(np.arange(0, 6, 0.5), np.arange(0, 6, 0.5))
+interp = interpolate.interp2d(X, Y, dx0.distancemap, kind='linear')
+Zi = interp(np.arange(0, 6, 0.5), np.arange(0, 6, 0.5)).reshape(Xi.shape)
 
-# Z = interp(np.arange(-0.5, 7), np.arange(-0.5, 7)).reshape((8,8))
-# print(Z)
+# now add the obstacles back in
+Zi[4:10, 6:10] = np.nan
+Zi[6:10, 4:6] = np.nan
 
-# put the NaNs back
-# for row, col in zip(*np.where(np.isnan(dx.distancemap))):
-#     Z[row+1, col+1] = 10 #np.nan
-# Z[3,3] = np.nan
+ax.plot_surface(Xi, Yi, Zi)
 
-# ax.plot_surface(Xi, Yi, Z, cmap='gray', 
-# antialiased=False, rstride=1, cstride=1,
-# vmin=0, vmax=10,
-#                                linewidth=1) 
-                               
-                               #, antialiased=True)
-
-# cset = ax.contourf(X, Y, dx.distancemap, 100, zdir='z', offset=0.0, cmap='gray')
-fc = np.zeros((6, 6, 4))
-fc[..., 3] = 1
-for row in range(6):
-    for col in range(6):
-        if np.isnan(dx.distancemap[row, col]):
-            fc[row, col, :3] = (1, 0, 0)
-        else:
-            fc[row, col, :3] = dx.distancemap[row, col] / 10
-ax.plot_surface(X, Y, 0*X, facecolors=fc, rstride=1, cstride=1, antialiased=True, shade=False)
-path = dx.query((5, 4))
-
-for p in path:
-    x = p[0]
-    y = p[1]
-    z = dx.distancemap[y, x] + 0.4
-    plt.plot([x, x], [y, y], [0, z], 'b')
-    base.plot_sphere(0.2, centre=(x, y, z), color='b', zorder=20)
-
-# ax.set_box_aspect([1, 1, 1])
-
-
-# plt.plot(x, y, z, 'ro-', zorder=20)
-
+ax.view_init(37, -151)
+ax.set_xlim(0, 6)
+ax.set_ylim(0, 6)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('distance from goal')
+ax.set_box_aspect((1, 1, 0.7))
 rvcprint.rvcprint(subfig='a')
 
 # ------------------------------------------------------------------------- #
