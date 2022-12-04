cde0b7ea7b709675d3d09d4bfba26223905c9f60
Peter Corke <peter.i.corke@gmail.com>
2022-12-05 07:57:41 +1100

    cleanup, add keywords, fix dependencies

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, find_packages
 import os
 
-# fmt: off
-import pip
-pip.main(['install', 'numpy>=1.17.4'])
-import numpy
-# fmt: on
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 req = [
-    "numpy>=1.17.4",
-    "scipy",
     "matplotlib",
-    "roboticstoolbox-python",
+    "roboticstoolbox-python >= 1",
     "machinevision-toolbox-python",
+    "bdsim",
+    "IPython",
+    "sympy",
+]
+
+pytorch_req = [
+    "torch",
+    "torchvision",
 ]
 
 # Get the long description from the README file
@@ -22,8 +23,8 @@ with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="RVC3-python",
-    version="0.1.0",
+    name="rvc3python",
+    version="0.2.2",
     description="Support for book: Robotics, Vision & Control 3 in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -41,22 +42,40 @@ setup(
         "License :: OSI Approved :: MIT License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     project_urls={
         "Documentation": "https://petercorke.github.io/RVC3-python",
         "Source": "https://github.com/petercorke/RVC3-python",
         "Tracker": "https://github.com/petercorke/RVC3-python/issues",
     },
-    keywords="python robotics robot-manipulator mobile-robot computer-vision",
-    packages=find_packages(exclude=["tests", "notebooks"]),
+    keywords=["robotics", "robot", "manipulator", "robot arm", 
+        "mobile robot", "mobile manipulation",
+        "path planning", "SLAM", "pose graph", 
+        "Dubins", "Reeds-Shepp", "lattice planner", "RRT", "PRM",
+        "rapidly exploring random tree", "probabilistic roadmap planner",
+        "force control", "kinematics", "Jacobian", "position control", "velocity control",
+        "spatial math", 
+        "SO(2)", "SE(2)", "SO(3)", "SE(3)",
+        "twist", "product of exponential", "translation", "orientation",
+        "angle-axis", "Lie group", "skew symmetric matrix",
+        "pose", "translation", "rotation matrix", "rigid body transform", "homogeneous transformation",
+        "Euler angles", "roll-pitch-yaw angles", "quaternion", "unit-quaternion"
+        "computer vision", "machine vision", "robotic vision",
+        "color space", "blackbody", "image segmentation", "blobs",
+        "Hough transform", "k-means", "homography", "camera calibration", "visual odometry",
+        "bundle adjustment",  "stereo vision", "rectification"
+        ],
+    #packages=["RVC3"],
+    packages=find_packages(),
     # package_data={"roboticstoolbox": extra_files},
-    scripts=["RVC3/bin/rvctool"],
+    # include_package_data=False,
+    scripts=["RVC3/bin/rvctool", "RVC3/bin/bdsim_path"],
     install_requires=req,
     extras_require={
+            "pytorch": pytorch_req,
     },
 )
